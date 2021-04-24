5dbac6b65694e83e65371bac14d7689cbb235afa
sinasafari <m.sinasafai@gmail.com>
2021-04-24 18:04:24 +0430

    Updates

--- src/App.js
+++
@@ -1,25 +0,0 @@
-import './App.css';
-import svg from './asset/coming-soon.539770ed.svg'
-
-function App() {
-    return (
-        <div className="container">
-
-            <div className="img-container text-center">
-                <h1 className="mt-5">
-                    🚀
-                </h1>
-                <h1>
-                    به زودی همراه شماییم
-                </h1>
-                <p className="mt-1">
-                    ما در حال ساخت یک پلتفرم برای دونیت کردن پروژه های شما هستیم، لطفا منتظر باشید.
-                </p>
-                <img className="mt-1"
-                     src={svg}/>
-            </div>
-        </div>
-    );
-}
-
-export default App;
